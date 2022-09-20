acaef173f08dbf524b874e7021cc4382674a6495
Slav <itiksolution@gmail.com>
2022-09-21 09:22:00 +1000

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+  return customerName
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+  return bestCustomer
+}
+console.log(bestCustomer)
+console.log(setBestCustomer())
+
+function overwriteBestCustomer(params) {
+  return (bestCustomer = 'maybe bob')
+}
+console.log(bestCustomer)
+
+function changeLeastFavoriteCustomer() {
+  const leastFavoriteCustomer = 'Bob'
+  leastFavoriteCustomer = 'bob'
+}
+console.log(changeLeastFavoriteCustomer())
